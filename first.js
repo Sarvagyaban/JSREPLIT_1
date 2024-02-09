0714cd76ab41c9de61512ac603831eb1213e0629
SARVAGYA BANSAL <81100088+Sarvagyaban@users.noreply.github.com>
2024-02-09 22:29:00 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,29 @@
+// Q1
+let name = "SARVAGYA";
+let dob = 16;
+console.log(name + dob);
+
+// Q2
+console.log(typeof (name + dob));
+
+// Q3
+const varr = {
+  name: "SARVAGYA",
+  dob: 16,
+};
+
+// Q4
+varr["friend"] = "SHIV";
+console.log(varr);
+varr["name"] = "SARVAGYABANSAL";
+console.log(varr);
+
+// Q5
+const dict = {
+  word1: "mean1",
+  word2: "mean2",
+  word3: "mean3",
+  word4: "mean4",
+  word5: "mean5",
+};
+console.log(dict["word1"]);
